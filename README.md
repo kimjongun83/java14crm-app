8f1602b945b453cef3c962791cc3a48187a5422e
kimjongun83 <quocdattranhuu1606@gmail.com>
2021-11-17 18:54:07 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# java14crm-gv
+ JAVA14 CRM APP
---
+++ src/main/java/cybersoft/javabackend/java14/com/WelcomeServlet.java
@@ -0,0 +1,20 @@
+package cybersoft.javabackend.java14.com;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name="welcomeServlet" ,urlPatterns = "/welcome")
+public class WelcomeServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().append("Welcome to CRM app !");
+	}
+
+}
